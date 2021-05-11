1f91a616b87fb8b77e33f8a13ec5c24f698231fa
Purdbull <72924017+Purdbull@users.noreply.github.com>
2021-05-11 20:40:44 +0200

    replaced temp variable renter points with new query method getTotalFrequentRenterPoints

--- src/MovieRenting/Customer.java
+++ src/MovieRenting/Customer.java
@@ -17,6 +17,15 @@ class Customer {
         return result;
     }
 
+    private int getTotalFrequentRenterPoints(){
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
 
     double amountFor(Rental aRental) {
         return aRental.getCharge();
@@ -33,24 +42,18 @@ class Customer {
     };
 
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration enum_rentals = _rentals.elements();
         String result = "MovieRenting.Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = (Rental) enum_rentals.nextElement();
-            //determine amounts for each line
-            thisAmount = each.getCharge();
-            // add frequent renter points
-            frequentRenterPoints = each.getFrequentRenterPoints();
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + each.getCharge() + "\n";
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
     }
 }
