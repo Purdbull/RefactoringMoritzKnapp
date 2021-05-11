53ee0ab015b53df90539abfaf031d85dbdde50d5
Purdbull <72924017+Purdbull@users.noreply.github.com>
2021-05-11 20:30:12 +0200

    extracted renter points

--- src/MovieRenting/Rental.java
+++ src/MovieRenting/Rental.java
@@ -7,6 +7,14 @@ class Rental {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+    int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
+    }
+
     double getCharge() {
         double result = 0;
         switch (getMovie().getPriceCode()) {
